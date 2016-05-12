6340b9571c57caafaa876a27a806b2703b6323b5
Dustin Schaerer <dustinschaerer@gmail.com>
2016-05-12 12:19:30 -0700

    Add nvm, node, webpack config, and npm packages - WIP

---
+++ app/assets/javascripts/greet.jsx
@@ -0,0 +1,11 @@
+this.Greet = React.createClass({
+  render: function() {
+    return (
+      <h1>Yo Im GREET Component</h1>
+    );
+  }
+
+});
+
+
+
---
+++ app/assets/javascripts/main.jsx
@@ -0,0 +1,24 @@
+this.Main = React.createClass({
+  render: function() {
+    return (
+      <Greet />
+    );
+  }
+});
+
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <h1>Yo Im the main Component</h1>
+//     );
+//   }
+// }
+
+// let documentReady = () => {
+//   ReactDOM.render(
+//     <Main />,
+//     document.getElementById('react')
+//   );
+// };
+
+// $(documentReady);
